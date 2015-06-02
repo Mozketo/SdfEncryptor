6317935f88d94f22ea5b0a3bb209bde89a914f9b
Ben Clark-Robinson <ben.clarkrobinson@gmail.com>
2015-06-02 11:40:02 +1000

    Added support for removing a password too

--- SdfEncryptor/Program.cs
+++ SdfEncryptor/Program.cs
@@ -8,47 +8,48 @@ namespace SdfEncryptor
     {
         static void Main(string[] args)
         {
-            if (args.Count() != 2)
+            if (args.Count() < 2)
                 Usage();
 
             var filename = args[0];
-            var password = args[1];
+            var newPassword = args[1];
+            if (newPassword.Equals("empty", StringComparison.InvariantCultureIgnoreCase))
+                newPassword = String.Empty;
+
+            var oldPassword = String.Empty;
+            if (args.Count() == 3)
+                oldPassword = args[2];
 
             if (File.Exists(filename) == false)
                 Usage($"Unable to find file {filename}");
 
-            CompactAndProtectSqlCeFile(filename, password);
+            CompactAndProtectSqlCeFile(filename, newPassword, oldPassword);
         }
 
-        static void CompactAndProtectSqlCeFile(string fileName, string password)
+        static void CompactAndProtectSqlCeFile(string fileName, string newPassword, string oldPassword = "")
         {
-            using (var sqlCeEngine = new System.Data.SqlServerCe.SqlCeEngine(ConnectionString(fileName)))
+            using (var sqlCeEngine = new System.Data.SqlServerCe.SqlCeEngine(ConnectionString(fileName, oldPassword)))
             { 
                 Console.WriteLine($"Compacting new database ({fileName}, {new FileInfo(fileName).Length} bytes). Please wait as this may take several minutes.");
                 sqlCeEngine.Compact(null);
-
-                if (password.HasValue())
-                {
-                    Console.WriteLine($"Encrypting with Password. This will take several minutes.");
-                    sqlCeEngine.Compact(ConnectionString(fileName, password));
-                    Console.WriteLine($"Database size is now {new FileInfo(fileName).Length} and encrypted.");
-                }
+                sqlCeEngine.Compact(ConnectionString(fileName, newPassword));
+                Console.WriteLine($"Done. Database size is now {new FileInfo(fileName).Length} and encrypted.");
             }
         }
 
-        static string ConnectionString(string sqlceDbFilePath, string password = null)
+        static string ConnectionString(string sqlceDbFilePath, string oldPassword = null)
         {
             var connectionString = $"Data Source=\"{sqlceDbFilePath}\";Max Database Size=4000";
-            if (password.HasValue())
+            if (oldPassword.HasValue())
             {
-                connectionString = connectionString + $";password=\"{password}\";Encryption Mode=Engine Default";
+                connectionString = connectionString + $";password=\"{oldPassword}\";Encryption Mode=Engine Default";
             }
             return connectionString;
         }
 
         static void Usage(string message = "")
         {
-            string usage = $"Usage: SdfEncryptor.exe [path] [key]. {message}";
+            string usage = $"Usage: SdfEncryptor.exe [path] [newpassword] [oldpassword]. {message}";
             Console.Error.WriteLine(usage);
             Environment.Exit(1);
         }
